b428d3421171f0a88a51fa0c33b6402024393f92
Vikas Chahar <misfityudi@gmail.com>
2024-09-21 04:59:04 +0530

    fighting against types

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,10 +2,10 @@
 // import { getServerSession } from "next-auth/next";
 // // import { authOptions } from "../auth/[...nextauth]/route";
 
-// export async function GET() {
-//   // const session = await getServerSession(authOptions);
-//   // if (!session) {
-//   //   return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
-//   // }
-//   // return NextResponse.json({ user: session.user });
-// }
+export async function GET() {
+  // const session = await getServerSession(authOptions);
+  // if (!session) {
+  //   return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
+  // }
+  // return NextResponse.json({ user: session.user });
+}

6f84d6db4bc12d4c1f57bf45b98986a4c874c29d
Vikas Chahar <misfityudi@gmail.com>
2024-09-21 05:10:31 +0530

    fighting lib/mongodb

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,29 +1,26 @@
 import { MongoClient } from "mongodb";
 
-// Define a custom global interface
-interface CustomNodeJsGlobal extends Node {
-  _mongoClientPromise?: Promise<MongoClient>;
+declare global {
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-// Declare global as custom interface
-declare const global: CustomNodeJsGlobal;
+const client = new MongoClient(process.env.MONGODB_URI!, {
+  // No need for deprecated options
+});
 
-// Create a new MongoClient instance
-const client = new MongoClient(process.env.MONGODB_URI as string);
-
-// Define a promise for the client connection
 let clientPromise: Promise<MongoClient>;
 
-// Check the environment and set up the client promise accordingly
 if (process.env.NODE_ENV === "development") {
-  if (!global._mongoClientPromise) {
-    global._mongoClientPromise = client.connect();
+  if (globalThis._mongoClientPromise) {
+    clientPromise = globalThis._mongoClientPromise;
+  } else {
+    clientPromise = client.connect().then((client) => {
+      globalThis._mongoClientPromise = client.connect();
+      return client;
+    });
   }
-  clientPromise = global._mongoClientPromise;
 } else {
-  // For production, connect directly
   clientPromise = client.connect();
 }
 
-// Export the client promise for use in other parts of the application
 export default clientPromise;
